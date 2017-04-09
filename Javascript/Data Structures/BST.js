34ee80c664cc0a0d478eb1df4e4bcda4d9067d12
Tyler Collins <tjscollins@gmail.com>
2017-04-09 10:37:41 +1000

    Fixed Javascript BST implementation

--- Javascript/Data Structures/BST.js
+++ Javascript/Data Structures/BST.js
@@ -1,29 +1,35 @@
 class BST {
   constructor() {
-    this.head = null;
-    this.size = 0;
+    this._head = null;
+    this._size = 0;
   }
   insert(data) {
-    if (this.head === null) {
-      this.head = this._newNode(data, null);
-      this.size++;
+    let {_newNode} = this;
+    if (data.key == undefined) {
+      throw new Error('Cannot create node without key value');
+    } else if (data.data == undefined) {
+      throw new Error('Cannot create node without data value');
+    }
+
+    if (this._head === null) {
+      this._head = _newNode(data, null);
+      this._size++;
     } else {
-      let node = this.head;
-      let parent = null;
+      let node = this._head;
       while (node !== null) {
-        parent = node;
-        if (data.key < node.key) {
-          if (node.left == null) {
-            node.left = this._newNode(data, node);
-            this.size++;
+        let {key} = node;
+        if(data.key < key) {
+          if(node.left === null) {
+            node.left = _newNode(data, node);
+            this._size++;
             node = null;
           } else {
             node = node.left;
           }
         } else {
-          if (node.right == null) {
-            node.right = this._newNode(data, node);
-            this.size++;
+          if(node.right === null) {
+            node.right = _newNode(data, node);
+            this._size++;
             node = null;
           } else {
             node = node.right;
@@ -33,8 +39,12 @@ class BST {
     }
   }
   search(key) {
-    let node = this.head;
-    while (node !== null) {
+    if (this._head === null) {
+      return null;
+    }
+
+    let node = this._head;
+    while(node !== null) {
       if (key < node.key) {
         node = node.left;
       } else if (key > node.key) {
@@ -43,16 +53,96 @@ class BST {
         return node;
       }
     }
-    return node;
+    return null;
   }
-  delete(key) {}
-  traverse() {}
-  prev(node) {}
-  next(node) {}
+  delete(key, Node) {
+    if (Node === undefined) {
+      // Find the relevant node
+      let node = this._head;
+      while(node !== null) {
+        if (key < node.key) {
+          node = node.left;
+        } else if (key > node.key) {
+          node = node.right;
+        } else {
+          break;
+        }
+      }
+      if (node === null) {
+        return; //Node does not exist
+      } else if (node === this._head) {
+        // throw new Error('Deleting head node');
+      }
+      Node = node;
+    }
+
+    if (Node.left === null || Node.right === null) {
+      if (Node === Node.parent.left) {
+        Node.parent.left = Node.left || Node.right;
+        if (Node.parent.left !== null) {
+          Node.parent.left.parent = Node.parent;
+        }
+      } else {
+        Node.parent.right = Node.left || Node.right;
+        if (Node.parent.right !== null) {
+          Node.parent.right.parent = Node.parent;
+        }
+      }
+      this._size--;
+      return Node;
+    } else {
+      let n = this._next(Node);
+      [n.key, Node.key] = [Node.key, n.key];
+      [n.data, Node.data] = [Node.data, n.data];
+      process.nextTick(() => {
+        this.delete(null, n);
+      });
+    }
+  }
+  traverse() {
+    let min = this._head;
+    while (min.left !== null) {
+      min = min.left;
+    }
+    let array = [{key: min.key, data: min.data}];
+    let current = this._next(min);
+    while (current !== null) {
+      array.push({key: current.key, data: current.data});
+      current = this._next(current);
+    }
+    return array;
+  }
+  _prev(node) {
+    if (node.left !== null) {
+      let n = node.left;
+      while (n.right !== null) {
+        n = n.right;
+      }
+      return n;
+    }
+    let current = node;
+    while (current.parent !== null && current === current.parent.left) {
+      current = current.parent;
+    }
+    return current.parent;
+  }
+  _next(node) {
+    if (node.right !== null) {
+      let n = node.right;
+      while (n.left !== null) {
+        n = n.left;
+      }
+      return n;
+    }
+    let current = node;
+    while (current.parent !== null && current === current.parent.right) {
+      current = current.parent;
+    }
+    return current.parent;
+  }
   _newNode(data, parent) {
     return {key: data.key, data: data.data, left: null, right: null, parent,};
   }
-  _appendBst(node, head) {}
 }
 
 module.exports = BST;
--- Javascript/spec/BST_spec.js
+++ Javascript/spec/BST_spec.js
@@ -1,149 +1,246 @@
-// const BST = require('../Data Structures/BST');
-//
-// describe('BST class: ', () => {
-//   it('should return a new BST object', () => {
-//     const bst = new BST();
-//
-//     expect(bst.head).toBe(null);
-//     expect(typeof bst.insert).toBe('function');
-//     expect(typeof bst.search).toBe('function');
-//     expect(typeof bst.delete).toBe('function');
-//     expect(typeof bst.traverse).toBe('function');
-//     expect(typeof bst.prev).toBe('function');
-//     expect(typeof bst.next).toBe('function');
-//   });
-//
-//   it('should maintain BST property after an arbitrary number of inserts', () => {
-//     const bst = new BST();
-//     for (let j = 0; j < 100; ++j) {
-//       for (let i = 0; i < 100; ++i) {
-//         bst.insert({
-//           key: Math.floor(Math.random() * 500),
-//           data: ' '
-//         });
-//       }
-//       expect(hasBstProp(bst.head)).toBe(true);
-//     }
-//   });
-//
-//   it('should search for and return a node if it exists', () => {
-//     const bst = new BST();
-//     for (let i = 0; i < 1000; ++i) {
-//       let key = Math.floor(Math.random() * 1000);
-//       if (key !== 123 && key !== 321 && key !== 234) {
-//         bst.insert({key, data: ' '});
-//       }
-//     }
-//     bst.insert({key: 123, data: 'a'});
-//     bst.insert({key: 321, data: 'b'});
-//     bst.insert({key: 234, data: 'c'});
-//
-//     expect(bst.search(123).data).toBe('a');
-//     expect(bst.search(321).data).toBe('b');
-//     expect(bst.search(234).data).toBe('c');
-//   });
-//
-//   it('should delete a node from the tree', () => {
-//     const bst = new BST();
-//     for (let i = 0; i < 1000; ++i) {
-//       let key = Math.floor(Math.random() * 1000);
-//       if (key !== 123) {
-//         bst.insert({key, data: ' '});
-//       }
-//     }
-//     bst.insert({key: 123, data: 'a'});
-//     expect(bst.search(123).data).toBe('a');
-//     bst.delete(123);
-//     expect(bst.search(123)).toBe(null);
-//   });
-//
-//   it('should maintain BST property after deletions', () => {
-//     for (let j = 0; j < 10; ++j) {
-//       let bst = new BST();
-//       for (let i = 0; i < 1000; ++i) {
-//         let key = Math.floor(Math.random() * 1000);
-//         bst.insert({key, data: ' '});
-//       }
-//       bst.delete(pickNode(bst));
-//       bst.delete(pickNode(bst));
-//       bst.delete(pickNode(bst));
-//       bst.delete(pickNode(bst));
-//       expect(hasBstProp(bst.head)).toBe(true);
-//       expect(bst.size).toBe(countBst(bst.head));
-//     }
-//   });
-//
-//   it('should support in-order traversal', () => {});
-//
-//   it('should find the predecessor of an arbitrary node', () => {
-//     let bst = new BST();
-//     bst.insert({key: 5, data: 5});
-//     bst.insert({key: 2, data: 2});
-//     bst.insert({key: 8, data: 8});
-//     bst.insert({key: 1, data: 1});
-//     bst.insert({key: 4, data: 4});
-//     bst.insert({key: 3, data: 3});
-//     bst.insert({key: 7, data: 7});
-//     bst.insert({key: 6, data: 6});
-//     bst.insert({key: 9, data: 9});
-//     bst.insert({key: 10, data: 10});
-//     for (let i = 10; i > 0; --i) {
-//       let node = bst.search(i);
-//       expect(bst.prev(node).key).toBe(i-1);
-//     }
-//   });
-//
-//   it('should find the successor of an arbitrary node', () => {
-//     let bst = new BST();
-//     bst.insert({key: 5, data: 5});
-//     bst.insert({key: 2, data: 2});
-//     bst.insert({key: 8, data: 8});
-//     bst.insert({key: 1, data: 1});
-//     bst.insert({key: 4, data: 4});
-//     bst.insert({key: 3, data: 3});
-//     bst.insert({key: 7, data: 7});
-//     bst.insert({key: 6, data: 6});
-//     bst.insert({key: 9, data: 9});
-//     bst.insert({key: 10, data: 10});
-//     for (let i = 1; i < 10; ++i) {
-//       let node = bst.search(i);
-//       // console.log(node);
-//       expect(bst.next(node).key).toBe(i+1);
-//     }
-//   });
-//
-// });
-//
-// function pickNode(bst) {
-//   let node = bst.head;
-//   let iter = Math.floor(Math.random()*Math.log(bst.size))
-//   let next = Math.floor(Math.random()*2) > 1 ? 'left' : 'right';
-//   while (node[next] != null && iter > 0) {
-//     next = Math.floor(Math.random()*2) > 1 ? 'left' : 'right';
-//     node = node[next]
-//   }
-//   return node !== null && node !== bst.head ? node.key : pickNode(bst);
-// }
-// function hasBstProp(node) {
-//   if (node == null) {
-//     return true;
-//   }
-//   let left = false,
-//     right = false;
-//   if (node.left == null || node.left.key < node.key) {
-//     left = true;
-//   }
-//   if (node.right == null || node.right.key >= node.key) {
-//     right = true;
-//   }
-//   return right && left && hasBstProp(node.left) && hasBstProp(node.right);
-// }
-//
-// function countBst(node) {
-//   if (node == null) {
-//     return 0;
-//   } else {
-//     return 1 + countBst(node.left) + countBst(node.right);
-//   }
-//
-// }
+const BST = require('../Data Structures/BST');
+
+describe('BST class: \n\t', () => {
+  describe('BST constructor: \n\t', () => {
+    it('should return a new BST object', () => {
+      const bst = new BST();
+
+      // Values
+      expect(bst._head).toBe(null);
+      expect(bst._size).toBe(0);
+
+      // Methods
+      // Insert a value into the tree
+      expect(typeof bst.insert).toBe('function');
+
+      // Search for and return a node if it exists
+      expect(typeof bst.search).toBe('function');
+
+      // Delete a node from the tree
+      expect(typeof bst.delete).toBe('function');
+
+      // Return an in-order list of all stored values
+      expect(typeof bst.traverse).toBe('function');
+
+      // Private method to create new nodes
+      expect(typeof bst._newNode).toBe('function');
+
+      // Private predecessor method
+      expect(typeof bst._prev).toBe('function');
+
+      // Private successor method
+      expect(typeof bst._next).toBe('function');
+    });
+  });
+
+  describe('BST.prototype.insert: \n\t', () => {
+      it('should maintain BST property after an arbitrary number of inserts', () => {
+        for (let j = 0; j < 100; ++j) {
+          let bst = new BST();
+          for (let i = 0; i < 100; ++i) {
+            bst.insert({
+              key: Math.floor(Math.random() * 500),
+              data: ' '
+            });
+          }
+          expect(bst._size).toBe(100);
+          expect(bst._head).not.toBe(null);
+          expect(hasBstProp(bst.head)).toBe(true);
+        }
+      });
+
+      it('should throw an error if the argument lacks a key value', () => {
+        const bst = new BST();
+        let errorThrown = false;
+        try {
+          bst.insert({data: 123});
+        } catch (error) {
+          expect(error).toBeDefined();
+          errorThrown = error.message === 'Cannot create node without key value';
+        }
+        expect(errorThrown).toBe(true);
+      });
+
+      it('should throw an error if the argument lacks a data value', () => {
+        const bst = new BST();
+        let errorThrown = false;
+        try {
+          bst.insert({key: 123});
+        } catch (error) {
+          expect(error).toBeDefined();
+          errorThrown = error.message === 'Cannot create node without data value';
+        }
+        expect(errorThrown).toBe(true);
+      });
+  });
+
+  describe('BST.prototype.search: \n\t', () => {
+      it('should search for and return a node if it exists', () => {
+        const bst = new BST();
+        for (let i = 0; i < 1000; ++i) {
+          let key = Math.floor(Math.random() * 1000);
+          if (key !== 123 && key !== 321 && key !== 234) {
+            bst.insert({key, data: ' '});
+          }
+        }
+        bst.insert({key: 123, data: 'a'});
+        bst.insert({key: 321, data: 'b'});
+        bst.insert({key: 234, data: 'c'});
+
+        expect(bst.search(123).data).toBe('a');
+        expect(bst.search(321).data).toBe('b');
+        expect(bst.search(234).data).toBe('c');
+      });
+
+      it('should return null if the target node does not exist', () => {
+        const bst = new BST();
+        for (let i = 0; i < 1000; ++i) {
+          let key = Math.floor(Math.random() * 1000);
+          if (key !== 123 && key !== 321 && key !== 234) {
+            bst.insert({key, data: ' '});
+          }
+        }
+        expect(bst.search(123)).toBe(null);
+        expect(bst.search(321)).toBe(null);
+        expect(bst.search(234)).toBe(null);
+      });
+  });
+
+  describe('BST.prototype.delete: \n\t', () => {
+      it('should delete a node from the tree', () => {
+        const bst = new BST();
+        for (let i = 0; i < 1000; ++i) {
+          let key = Math.floor(Math.random() * 10000);
+          if (key !== 123 && key !== 321 && key !== 234) {
+            bst.insert({key, data: ' '});
+          }
+        }
+        bst.insert({key: 123, data: 'a'});
+        bst.insert({key: 321, data: 'b'});
+        bst.insert({key: 234, data: 'c'});
+        expect(bst.search(123).data).toBe('a');
+        expect(bst.search(321).data).toBe('b');
+        expect(bst.search(234).data).toBe('c');
+        bst.delete(123);
+        expect(bst.search(123)).toBe(null);
+        bst.delete(321);
+        expect(bst.search(321)).toBe(null);
+        bst.delete(234);
+        expect(bst.search(234)).toBe(null);
+      });
+
+      it('should maintain BST property after deletions', () => {
+        for (let j = 0; j < 10; ++j) {
+          let bst = new BST();
+          for (let i = 0; i < 1000; ++i) {
+            let key = Math.floor(Math.random() * 10000);
+            bst.insert({key, data: ' '});
+          }
+          bst.delete(pickNode(bst));
+          bst.delete(pickNode(bst));
+          bst.delete(pickNode(bst));
+          bst.delete(pickNode(bst));
+          expect(hasBstProp(bst._head)).toBe(true);
+          expect(bst._size).toBe(countBst(bst._head));
+        }
+      });
+  });
+
+  describe('BST.prototype.traverse: \n\t', () => {
+    it('should return a sorted array of all the nodes', () => {
+        let bst = new BST();
+        bst.insert({key: 5, data: 5});
+        bst.insert({key: 2, data: 2});
+        bst.insert({key: 8, data: 8});
+        bst.insert({key: 1, data: 1});
+        bst.insert({key: 4, data: 4});
+        bst.insert({key: 3, data: 3});
+        bst.insert({key: 7, data: 7});
+        bst.insert({key: 6, data: 6});
+        bst.insert({key: 9, data: 9});
+        bst.insert({key: 10, data: 10});
+        let array = bst.traverse().map(node => node.key);
+        expect(array).toEqual(array.sort((a,b) => a-b));
+    });
+  });
+
+  describe('BST.prototype private methods: \n\t', () => {
+    it('should find the predecessor of an arbitrary node', () => {
+      let bst = new BST();
+      bst.insert({key: 5, data: 5});
+      bst.insert({key: 2, data: 2});
+      bst.insert({key: 8, data: 8});
+      bst.insert({key: 1, data: 1});
+      bst.insert({key: 4, data: 4});
+      bst.insert({key: 3, data: 3});
+      bst.insert({key: 7, data: 7});
+      bst.insert({key: 6, data: 6});
+      bst.insert({key: 9, data: 9});
+      bst.insert({key: 10, data: 10});
+      for (let i = 10; i > 1; --i) {
+        let node = bst.search(i);
+        expect(bst._prev(node).key).toBe(i-1);
+      }
+    });
+
+    it('should find the successor of an arbitrary node', () => {
+      let bst = new BST();
+      bst.insert({key: 5, data: 5});
+      bst.insert({key: 2, data: 2});
+      bst.insert({key: 8, data: 8});
+      bst.insert({key: 1, data: 1});
+      bst.insert({key: 4, data: 4});
+      bst.insert({key: 3, data: 3});
+      bst.insert({key: 7, data: 7});
+      bst.insert({key: 6, data: 6});
+      bst.insert({key: 9, data: 9});
+      bst.insert({key: 10, data: 10});
+      for (let i = 1; i < 10; ++i) {
+        let node = bst.search(i);
+        expect(bst._next(node).key).toBe(i+1);
+      }
+    });
+  });
+});
+
+function pickNode(bst) {
+  let node = bst._head;
+  let iter = Math.floor(Math.random()*Math.log(bst._size))
+  let next = Math.floor(Math.random()*2) > 1 ? 'left' : 'right';
+  while (node[next] != null && iter > 0) {
+    next = Math.floor(Math.random()*2) > 1 ? 'left' : 'right';
+    node = node[next]
+  }
+  return node !== null && node !== bst._head ? node.key : pickNode(bst);
+}
+
+
+/**
+ * hasBstProp - Tests a bst node and its subtrees to verify that
+ *              they satisfy the bst property.
+ *
+ * @param  {object} node a bst node to be checked for the bst property
+ * @return {bool}      true if bst property holds, false if not
+ */
+function hasBstProp(node) {
+  if (node == null) {
+    return true;
+  }
+  let left = false,
+    right = false;
+  if (node.left == null || node.left.key < node.key) {
+    left = true;
+  }
+  if (node.right == null || node.right.key >= node.key) {
+    right = true;
+  }
+  return right && left && hasBstProp(node.left) && hasBstProp(node.right);
+}
+
+function countBst(node) {
+  if (node == null) {
+    return 0;
+  } else {
+    return 1 + countBst(node.left) + countBst(node.right);
+  }
+}
