7b4be248604166009572a736b4d5cb65594c756e
Tyler Collins <tjscollins@gmail.com>
2017-04-22 10:40:52 +1000

    Add qsort_inplace to python tests

--- Python/Tests/sorting-algos/quick_test.py
+++ Python/Tests/sorting-algos/quick_test.py
@@ -7,7 +7,7 @@ from random import randint
 mod_directory = [os.getcwd() + '/Sorting']
 mod_directory.extend(sys.path)
 sys.path = mod_directory
-from quick import qsort
+from quick import qsort, qsort_inplace
 
 class TestSort(unittest.TestCase):
     def test_sort(self):
@@ -16,6 +16,13 @@ class TestSort(unittest.TestCase):
             for i in range(1000):
                 array.append(randint(-100,100))
             self.assertListEqual(qsort(array), sorted(array))
+
+    def test_sort_inplace(self):
+        for j in range(25):
+            array = []
+            for i in range(1000):
+                array.append(randint(-100,100))
+            self.assertListEqual(qsort_inplace(array), sorted(array))
 
 def main():
     insert_sort_suite = unittest.TestLoader().loadTestsFromTestCase(TestSort)
